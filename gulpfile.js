c5407c8dac10878f7f542dd9b7b543914a6bccad
Julien Vernet <julien@vernet.me>
2015-10-23 11:01:50 +0700

    Minify CSS and watch for changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
+var minifyCss = require('gulp-minify-css');
 var minifyInline = require('gulp-minify-inline');
 var fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync').create();
@@ -16,6 +17,16 @@ gulp.task('copy', function () {
 		.pipe(gulp.dest('dist'));
 });
 
+// Minify stylesheets
+gulp.task('minify-css', function () {
+	return gulp.src('./src/app.css')
+		.pipe(minifyCss({
+			compatibility: 'ie8'
+		}))
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.stream());
+});
+
 // Minify inline scripts & HTML
 gulp.task('minify', function () {
 	return gulp.src('./src/**/*.html')
@@ -30,13 +41,14 @@ gulp.task('minify', function () {
 		.pipe(gulp.dest('dist'))
 });
 
-// Static Server + watching html files
-gulp.task('serve', function () {
+// Static Server + watching css/html files
+gulp.task('serve', ['minify-css'], function () {
 	browserSync.init({
 		server: './dist'
 	});
-	gulp.watch('src/*.html').on('change', browserSync.reload);
+	gulp.watch('./src/app.css', ['minify-css']);
+	gulp.watch('./src/*.html').on('change', browserSync.reload);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['copy', 'minify']);
+gulp.task('default', ['copy', 'minify-css', 'minify']);
